d9f84da5d4b5b9d5529150326b22108bfe3505fe
chetan14203 <109654816+chetan14203@users.noreply.github.com>
2024-10-14 09:58:50 +0530

    Your commit message here

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use('/',userRoutes)
 
-const uri = "mongodb://localhost:27017/myData";
+const uri = "mongodb+srv://choudharyc355:carmel@cluster0.zhf8t.mongodb.net/scrapify?retryWrites=true&w=majority&appName=Cluster0";
 console.log(uri);
 
 const connectToDatabase = async () => {
